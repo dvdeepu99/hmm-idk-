207077f68cdb0d499d4d2d8be9ef681434e224e2
Deepu <93904807+dvdeepu99@users.noreply.github.com>
2022-12-02 16:31:04 +0000

    these are tests and you should probably ignore this.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = 'hello'
+#this is a test
+name = 'Koyla'
 age = 55
 
 print("Hi {0}, your age is {1}. Good Morning.".format(name,age))
+print(f'Hi {name}, your age is {age}, Good Morning.')
+print("Hi " + name + ", your age is " + str(age) + ", Good Morning.")
