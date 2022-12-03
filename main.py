1c3079cbc5bdf9f5797c2919a135072cdddd6cbb
Deepu <93904807+dvdeepu99@users.noreply.github.com>
2022-12-03 05:05:47 +0000

    You should probably ignore this.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 name = 'Koyla'
 age = 55
 
-print("Hi {0}, your age is {1}. Good Morning.".format(name,age))
+print("Hi {0}, your age is {1}. Good Morning.".format(name, age))
 print(f'Hi {name}, your age is {age}, Good Morning.')
 print("Hi " + name + ", your age is " + str(age) + ", Good Morning.")
+print("He said \"Hello, <your name>, your age is <age>, Good Morning\"")
+year = int(input("What year were you born?"))
+age = 2022 - year
+print("Your age is " + str(age) + " Years")
