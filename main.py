650f7ba8b5f982c390fc2a3e72dba629a211d37d
Deepu <93904807+dvdeepu99@users.noreply.github.com>
2022-12-03 05:28:03 +0000

    You should probably ignore this.

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ print("He said \"Hello, <your name>, your age is <age>, Good Morning\"")
 year = int(input("What year were you born?"))
 age = 2022 - year
 print("Your age is " + str(age) + " Years")
+
+pwd = input("Enter your password")
+lth = len(pwd)
+print(lth)
+print(type(lth))
+
+i = 1
+pswd = ''
+for i in pwd:
+  pswd = (str(pswd) + '*')
+print(type(pswd))
+print(f"Your password {pswd} has a length of {lth} characters")
